b5d57adca1a39f92bf48486c7b5cbeb4b28b208f
Justin Holguin <justin.h.holguin@gmail.com>
2014-12-14 11:45:05 -0800

    Docstrings

--- src/suurvay/streaming_twitter.clj
+++ src/suurvay/streaming_twitter.clj
@@ -17,6 +17,11 @@
                   verified followers-count])
 
 (defn- tweet->rec
+  "DEPRECATED:
+  
+  Used to construct a Tweet object from a Status object. Since Tweet
+  objects will no longer be used, this is here only as a reference
+  during The Great Refactoring."
   [tweet]
   (let [id          (get-in tweet ["user" "id"])
         name        (get-in tweet ["user" "name"])
@@ -31,13 +36,15 @@
               name profile user-creation-date
               verified followers-count))))
 
-(sc/defn keywordize-tweet :- Status
+(sc/defn keywordize-tweet :- (sc/maybe Status)
+  "Quickly validates whether the given status is a usable tweet,
+  returning it if it is. Otherwise returns nil."
   [status]
   (let [{:strs [id text]} status]
     (when (and id text)
       (keywordize-keys status))))
 
-(defn cond>!
+(defn- cond>!
   "Places val onto the given async channel iff it is truthy."
   [port val]
   (when val
@@ -45,7 +52,15 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Auth
-(defn bf-creds
+(defn creds-list->map
+  "The `filter-stream` function requires a map of key -> token, which
+  this function can provide. Takes four arguments in the following
+  order:
+
+  1. consumer-key
+  2. consumer-secret
+  3. access-token
+  4. access-secret"
   [& creds-list]
   {:pre [(= 4 (count creds-list))]}
   (zipmap [:consumer-key :consumer-secret :access-token :access-secret] creds-list))
