3c0cdd56edc17f47aa0fd7198e9c7982da427d8f
Justin Holguin <justin.holguin@puppetlabs.com>
2015-04-25 14:06:32 -0700

    Parameterize storage tests
    This will dramatically simplify testing new implementations of the
    Storage protocol.

--- test/suurvay/storage_test.clj
+++ test/suurvay/storage_test.clj
@@ -8,23 +8,27 @@
 (def me (tw/get-user test-creds "postpunkjustin"))
 (def my-timeline (tw/get-timeline-details test-creds "postpunkjustin"))
 
+(defn test-storage-instance
+  "Runs a standard set of tests on the given instance of the Storage
+  protocol."
+  [stg]
+  (testing "can store and retrieve a user"
+    (is (nil? (store-user stg me)))
+    (let [retrieved (get-user stg (:id me))]
+      (is (= me retrieved))))
+
+  (testing "can store and retrieve a status object"
+    (let [status (first my-timeline)]
+      (is (nil? (store-status stg status)))
+      (let [retrieved (get-status stg (:id status))]
+        (is (= status retrieved)))))
+
+  (testing "can store and retrieve a timeline"
+    (is (nil? (store-timeline stg my-timeline)))
+    (let [retrieved (get-timeline stg (:id me))]
+      (is (= (set my-timeline)
+             (set retrieved))))))
+
 (deftest in-memory-storage-test
   (testing "an in-memory storage instance"
-    (let [stg (in-memory-only)]
-
-      (testing "can store and retrieve a user"
-        (is (nil? (store-user stg me)))
-        (let [retrieved (get-user stg (:id me))]
-          (is (= me retrieved))))
-
-      (testing "can store and retrieve a status object"
-        (let [status (first my-timeline)]
-          (is (nil? (store-status stg status)))
-          (let [retrieved (get-status stg (:id status))]
-            (is (= status retrieved)))))
-
-      (testing "can store and retrieve a timeline"
-        (is (nil? (store-timeline stg my-timeline)))
-        (let [retrieved (get-timeline stg (:id me))]
-          (is (= (set my-timeline)
-                 (set retrieved))))))))
+    (test-storage-instance (in-memory-only))))
